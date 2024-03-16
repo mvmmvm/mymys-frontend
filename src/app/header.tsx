8bbe9adfd67a0442f868d28ff44eb52588fe4815
Mai Sato <mu.mat.sig@gmail.com>
2024-03-16 21:51:33 +0900

    表示の修正

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,14 +6,14 @@ import {Typography} from "@mui/material";
 const Header = () => {
   return (
 
-    <header className="dark:bg-gray-900 top-0 w-full">
+    <header className="dark:bg-gray-900 top-0 w-full fixed z-50">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
           <Typography variant="h4" align="center" color="#69717f">
-          Murder Maker!!!
+            <Link href="/" passHref>Murder Maker!!!</Link>
           </Typography>
         </div>
       </nav>
