edab8d6099a2fbead244c4073426f7f6d43caa02
Mai Sato <mu.mat.sig@gmail.com>
2024-04-04 23:48:29 +0900

    modified

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,4 +1,12 @@
+const generateMailtoLink = (user: string, domain: string) => {
+  return `mailto:${user}@${domain}`
+}
+
 export default function Footer() {
+  const mailUser = 'mu.mat.sig'
+  const mailDomain = 'gmail.com'
+  const mailtoLink = generateMailtoLink(mailUser, mailDomain)
+  
   return (
     <footer className="bg-white dark:bg-gray-900 bottom-0 w-full fixed z-50">
     <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
@@ -11,8 +19,8 @@ export default function Footer() {
         </a>
       </div>
       <div className="mmd:mt-0 md:order-1">
-        <p className="text-center text-sm text-gray-400">&copy; 2024 Mai Sato</p>
-        <p className="text-center text-sm text-gray-400">※作成にはAIを使用しているので文章がおかしい場合がございますがご了承くださいますようお願いします。</p>
+        <a href={mailtoLink} className="text-center text-sm text-gray-400">&copy; 2024 Mai Sato</a>
+        <p className="text-center text-sm text-gray-400">※作成中表示のまま変わらない場合はリロードをお願いいたします。</p>
       </div>
     </div>
   </footer>
